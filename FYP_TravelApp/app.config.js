767e11562bfa871cb99b09a7c2c9ee76a589fb07
Mansoor-badar <mansoorbadar05@gmail.com>
2026-04-18 11:23:28 +0100

    Gradle Build Changes (#19)

--- FYP_TravelApp/app.config.js
+++ FYP_TravelApp/app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default {
   "expo": {
     "name": "FYP_TravelApp",
     "slug": "FYP_TravelApp",
@@ -15,7 +17,7 @@
     "ios": {
       "supportsTablet": true,
       "config": {
-        "googleMapsApiKey": "${EXPO_PUBLIC_GOOGLE_MAPS_KEY}"
+        "googleMapsApiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_KEY
       }
     },
     "android": {
@@ -26,15 +28,22 @@
       "edgeToEdgeEnabled": true,
       "config": {
         "googleMaps": {
-          "apiKey": "${EXPO_PUBLIC_GOOGLE_MAPS_KEY}"
+          "apiKey": process.env.EXPO_PUBLIC_GOOGLE_MAPS_KEY
         }
-      }
+      },
+      "package": "com.mansoorbadar.FYP_TravelApp"
     },
     "web": {
       "favicon": "./assets/favicon.png"
     },
     "plugins": [
-      "@react-native-community/datetimepicker"
-    ]
+      "@react-native-community/datetimepicker",
+      "expo-font"
+    ],
+    "extra": {
+      "eas": {
+        "projectId": "046d6151-3f3e-4cff-88e5-4afce46e985f"
+      }
+    }
   }
-}
+};
